3db8bd1e2b8bfe6b8a12875cd546fe7d9bcb423f
johnclary <john.clary@austintexas.gov>
2020-04-19 18:39:22 -0500

    implement create_tables

--- knackpostgres/loader.py
+++ knackpostgres/loader.py
@@ -2,24 +2,26 @@ import logging
 
 import psycopg2
 
-
 class Loader:
     """ Wrapper for executing Knack applicaton SQL commands """
-
     def __repr__(self):
         return f"<Loader {self.app.name}>"
 
     def __init__(self, app):
         self.app = app
 
-    def connect(
-        self, host="localhost", dbname="postgres", user="postgres", password=None
-    ):
+    def connect(self, host="localhost", dbname="postgres", user="postgres", password=None):
         if not password:
             raise AttributeError("`password` is required to connect.")
-
-        self.conn = psycopg2.connect(
-            host=host, dbname=dbname, user=user, password=password
-        )
-        logging.info(f"Connected to <{dbname}> as <{user}> at {host}")
+        
+        self.conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+        self.conn.autocommit = True
         return self.conn
+
+    def create_tables(self):
+        
+        with self.conn.cursor() as cursor:
+            for table in self.app.tables:
+                print(table)
+                cursor.execute(table.sql)
+                logging.info(f"Created {table}")
