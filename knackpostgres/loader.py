ae3a1677a3fb9b86d94c183bfa9deff5608a4f30
johnclary <john.clary@austintexas.gov>
2020-04-19 19:04:47 -0500

    new execute_sql method

--- knackpostgres/loader.py
+++ knackpostgres/loader.py
@@ -10,8 +10,11 @@ class Loader:
     def __repr__(self):
         return f"<Loader {self.app.name}>"
 
-    def __init__(self, app):
+    def __init__(self, app, overwrite=False):
         self.app = app
+
+        # if true, will drop entire public schema from dest database!
+        self.overwrite = overwrite
 
     def connect(
         self, host="localhost", dbname="postgres", user="postgres", password=None
@@ -26,13 +29,26 @@ class Loader:
         return self.conn
 
     def create_tables(self):
+
         with self.conn.cursor() as cursor:
+            if self.overwrite:
+                cursor.execute("DROP SCHEMA public CASCADE;")
+                cursor.execute("CREATE SCHEMA public;")
+
             for table in self.app.tables:
-                cursor.execute(table.sql)
-                logging.info(f"Created {table}")
+                self._execute_sql(cursor, table.sql, table.name)
 
     def create_relationships(self):
         with self.conn.cursor() as cursor:
             for rel in self.app.sql_relationships:
-                cursor.execute(rel["sql"])
-                logging.info(f"Created {rel['name']}")
+                self._execute_sql(cursor, rel["sql"], rel["name"])
+
+    def _execute_sql(self, cursor, sql, name):
+        try:
+            cursor.execute(sql)
+            logging.info(f"Created {name}")
+
+        except psycopg2.ProgrammingError as e:
+            logging.error(f"Failed to create {name}")
+            logging.error(e)
+            pass
