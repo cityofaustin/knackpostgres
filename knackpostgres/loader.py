49dcb829b6dbe0b3c31ab3cf1715990dbbe065ce
johnclary <john.clary@austintexas.gov>
2020-05-24 00:44:50 -0500

    better warning msg

--- knackpostgres/loader.py
+++ knackpostgres/loader.py
@@ -37,7 +37,7 @@ class Loader:
             return None
 
         print(
-            "\n!! You are about to overwrite the public schema in the destination database !!"
+            f"\n!! You are about to overwrite the `{self.app.schema}` and `{self.app.metadata_schema}` schema in the destination database !!"
         )
         confirmed = input('Type "yes"` to continue.\n')
 
