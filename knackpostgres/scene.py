a22cbaca4e2df303c267d51123c2443e1a02bc65
johnclary <john.clary@austintexas.gov>
2020-05-25 08:45:54 -0500

    begin pages work in earnest

--- knackpostgres/scene.py
+++ knackpostgres/scene.py
@@ -1,27 +1,37 @@
 from pprint import pprint as print
 
 from .view_knack import ViewKnack
+from knackpostgres.config.metadata import METADATA_FIELDS
 from knackpostgres.utils.utils import valid_pg_name
+    
+import pdb
+
+# NOT IMPLEMENTED SCENE ATTRS
+# unsupported_scene_attrs = [
+#     'authentication_profiles_knack',
+#     'ignore_entry_scene_menu_knack',
+#     'print_knack',
+#     'modal_knack',
+#     'page_menu_display_knack',
+# ]
 
 
 class Scene:
     """ Base class for Knack `scene` definition wrappers """
 
     def __repr__(self):
-        return f"<{type(self).__name__} {self.name_knack} {self.type_knack}>"
+        return f"<{type(self).__name__} {self.name}>"
 
     def __init__(self, data):
-        """
-        No knack field is used as a primary key. 
-        We generate a primary key field while handling each table, during
-        which we set primary_key = `True`
-        """
+        scene_attrs = [field["name"] for field in METADATA_FIELDS["_pages"]]
+
         for key in data:
-            setattr(self, key + "_knack", data[key])
+            if key in scene_attrs:
+                setattr(self, key, data[key])
 
         self._handle_views()
 
     def _handle_views(self):
-        self.views = []
-        for view in self.views_knack:
-            self.views.append(ViewKnack(view))
+        self._views = []
+        for view in self.views:
+            self._views.append(ViewKnack(view))
--- knackpostgres/view_knack.py
+++ knackpostgres/view_knack.py
@@ -1,18 +1,37 @@
 from pprint import pprint as print
+from knackpostgres.config.metadata import METADATA_FIELDS
 from knackpostgres.utils.utils import valid_pg_name
 
+import pdb
+
+
+# NOT IMPLEMENTED ATTRIBUTES
+# ignored_view_attributes = [
+#     "links",
+#     'action',
+#     'display_pagination_below',
+#     'allow_preset_filters',
+#     'registration_type',
+#     'hide_empty',
+#     'filter_fields',
+#     'totals',
+#     'no_data_text',
+#     'rows_per_page',
+#     'allow_limit',
+#     'menu_filters',
+#     'preset_filters',
+#     "filter_type",
+# ]
 
 class ViewKnack:
     """ Base class for Knack `scene` definition wrappers """
 
     def __repr__(self):
-        return f"<{type(self).__name__} {self.name_knack} {self.type_knack}>"
+        return f"<{type(self).__name__} {self.name} {self.type}>"
 
     def __init__(self, data):
-        """
-        No knack field is used as a primary key. 
-        We generate a primary key field while handling each table, during
-        which we set primary_key = `True`
-        """
+        view_attrs = [field["name"] for field in METADATA_FIELDS["_views"]]
+
         for key in data:
-            setattr(self, key + "_knack", data[key])
+            if key in view_attrs:
+                setattr(self, key, data[key])
